6aae9fa73fbab8c1554370e6017c0efefd4b3d34
gabeschulman <gs4342@nyu.edu>
2025-11-24 21:46:08 -0500

    small cleanup

--- src/trainer.py
+++ src/trainer.py
@@ -402,8 +402,8 @@ def main(job_id: int, config_name: str):
                 else:
                     torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
                     optimizer.step()
-                scheduler.step()
                 optimizer.zero_grad()
+                scheduler.step()
 
             total_train_loss += loss.item() * gradient_accumulation_steps
 
